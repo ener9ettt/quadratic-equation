a5f5c505ce0dde0e908b07e22183e56a435f5159
Alexei Lashakov <alexei.lashakov@mail.ru>
2018-09-25 15:39:47 +0300

    Added implementation of function solveEquation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var coefficients = equation.replace("^2", " * x"); // obtaining an array of coefficients of the quadratic equation
+    coefficients = coefficients.replace(/\s/g, '');
+    coefficients = coefficients.match(/[^*x^+]+/g);
+    coefficients = coefficients.map(string => +string);
+
+    var [a, b, c] = coefficients;
+
+    var x = [];
+    x[1] = Math.round((- b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a));
+    x[0] = Math.round((- b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a));
+    x = x.sort((a, b) => a - b);
+    return x;
 }
